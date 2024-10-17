befe06ad7af61ead46cb60c721cbe11f6a28609a
AfawaEQ <and.eyp@yandex.ru>
2024-10-17 20:48:54 +0300

    Добавлена докумментация ко всем функциям

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус r, возвращает площадь окружности радиуса r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус r, возвращает периметр окружности радиуса r'''
     return 2 * math.pi * r
 
