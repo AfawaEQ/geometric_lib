f913997b247c44acb4ff206d250f97294ef5ffda
AfawaEQ <and.eyp@yandex.ru>
2024-10-30 18:30:46 +0300

    Добавлены классы

--- circle.py
+++ circle.py
@@ -1,12 +1,11 @@
 import math
 
+class Circle:
+    def area(r):
+        '''Принимает радиус r, возвращает площадь окружности радиуса r'''
+        return math.pi * r * r
 
-def area(r):
-    '''Принимает радиус r, возвращает площадь окружности радиуса r'''
-    return math.pi * r * r
-
-
-def perimeter(r):
-    '''Принимает радиус r, возвращает периметр окружности радиуса r'''
-    return 2 * math.pi * r
+    def perimeter(r):
+        '''Принимает радиус r, возвращает периметр окружности радиуса r'''
+        return 2 * math.pi * r
 
